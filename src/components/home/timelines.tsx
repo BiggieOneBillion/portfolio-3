d79bdc92715bf59ab2fd1d6855a4030b3c70d232
BiggieOneBillion <chukwuchinwendu94@gmail.com>
2024-11-24 21:30:32 +0100

    timeline edit -2

--- src/components/home/timelines.tsx
+++ src/components/home/timelines.tsx
@@ -85,7 +85,7 @@ const dummyContent = [
         </p>
       </>
     ),
-    badge: "Early 2023",
+    badge: "Early 2024",
     image:
       "https://images.unsplash.com/photo-1519681393784-d120267933ba?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
